5f04980636f425d8229935d5efff77c0910099e8
Yangel300 <yangel@unal.edu.co>
2025-04-21 15:54:32 -0500

    Final program CPP

--- Trabajo1/Sumatoria1.cpp
+++ Trabajo1/Sumatoria1.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 #include <cmath>
 
-typedef double Real;
+typedef float Real;
 
 Real sum1(int k);
 Real sum2(int k);
 Real sum3(int k);
+Real error(Real term1, Real term2);
+
 
 int main(void){
     std::cout.precision(7);
     std::cout.setf(std::ios::scientific);
 
-    for (int k = 1; k <= 100000; k++) {
-        std::cout << k << "\t" << sum1(k) << "\t"<< sum2(k) << "\t" <<sum3(2*k-1)<<"\n";
+    for (int k = 1; k <= 25000; k++) {
+        std::cout << k << "\t" << sum1(2*k-1) << "\t"<< sum2(k) << "\t" <<sum3(k)<<"\t"<<error(sum1(2*k-1),sum3(k))<<"\t"<< error(sum2(k),sum3(k))<<"\n";
     }
 
     return 0;
 }
 
 
-Real sum3(int k){
+Real sum1(int k){
     Real suma=0.0;
     for (int i=1;i<k;i++){
         Real term4=pow(-1,i)*(i/(i+1.0));
@@ -38,11 +40,16 @@ Real sum2(int k){
     return suma;
 }
 
-Real sum1(int k){
+Real sum3(int k){
     Real suma=0.0;
     for (int i=1;i<k;i++){
         Real term=1.0/(2*i*(2*i+1.0));
         suma+=term;
     }
     return suma;
+}
+
+Real error(Real term1, Real term2){
+    Real error=fabs((term1-term2)/term2);
+    return error;
 }
